fabaac13bb4a80872989f23f00a044ba2d2cbb46
王浩 <hey-xiaohao@163.com>
2014-09-24 22:54:26 +0800

    添加分布式进程.py知识归纳

--- 多线程.py
+++ 多线程.py
@@ -23,5 +23,35 @@ if __name__ == '__main__':
 
 # 锁 lock = threading.Lock()  lock.acquire()  lock.release
 
-# ThreadLocal 解决不同线程处理同一个对象时  免去 传参 及 全局变量的方式、
+# ThreadLocal 解决不同线程处理同一个对象时  免去 传参 及 全局变量的方式、  常用于：每个线程绑定一个数据库连接，HTTP请求，用户身份信息等
+# 创建全局ThreadLocal对象 
+local_school = threading.local()
+
+def process_student():
+    print 'Hello %s (in %s)' % (local_school.student, threading.current_thread().name)
+
+def process_thread(name):
+    # 绑定TrheadLocal的student
+    local_school.student = name
+    process_student()
+
+t1 = threading.Thread(target = process_thread, args = ('wangh',), name = 'ThreadA')
+t2 = threading.Thread(target = process_thread, args = ('kongl',), name = 'ThreadB')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
