ae76ff963ac2570e65a99ad2f1bc216d13141b47
王浩 <hey-xiaohao@163.com>
2014-09-26 10:42:47 +0800

    添加数据库编程.py

---
+++ 数据库编程.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+type = 'sqlite3'
+
+if type == 'sqlite3':
+    # 导入驱动
+    import sqlite3
+    # 连接到数据库 test.db 如果文件不存在，则会自动创建
+    conn = sqlite3.connect('test.db')
+    # 创建一个cursor
+    cursor = conn.cursor()
+    # 执行创建表
+    cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+    # 执行插入数据
+    cursor.execute("insert into user (id, name) values ('1', 'wangh')")
+    # 通过rowcount获取影响的行数
+    print cursor.rowcount
+    # 关闭cursor
+    cursor.close()
+    # 提交事物
+    conn.commit()
+    # 关闭connection
+    conn.close()
+    
+    conn1 = sqlite3.connect('test.db')
+    cursor1 = conn1.cursor()
+    # 执行查询语句
+    cursor1.execute("select * from user where id=?", '1')
+    # 获取结果集
+    value = cursor1.fetchall()
+    print value
+    cursor1.close()
+    conn1.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
